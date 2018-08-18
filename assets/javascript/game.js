6af7f5b3c3cbda28fa652d302812b43017092f3d
Anastasiya Tanas <Anastasiya@Anastasiyas-MacBook.local>
2018-08-17 18:28:57 -0600

    final push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,64 +10,70 @@ var loses = 0;
 
 
 
-randomNum = Math.floor((Math.random() * 101)+ 19);
+randomNum = Math.floor((Math.random() * 101) + 19);
 console.log(randomNum);
 
-crystal1 = Math.floor((Math.random() * 11)+ 1);//random numbers for crystalmeth
+crystal1 = Math.floor((Math.random() * 11) + 1); //random numbers for crystalmeth
 console.log(crystal1);
-crystal2 = Math.floor((Math.random() * 11)+ 1);
+crystal2 = Math.floor((Math.random() * 11) + 1);
 console.log(crystal2);
-crystal3 = Math.floor((Math.random() * 11)+ 1);
+crystal3 = Math.floor((Math.random() * 11) + 1);
 console.log(crystal3);
-crystal4 = Math.floor((Math.random() * 11)+ 1);
+crystal4 = Math.floor((Math.random() * 11) + 1);
 console.log(crystal4);
 
 
 
 $(document).ready(function () {
-//to print random number
-$("#randomNum").text(randomNum);
+    //to print random number
+    $("#randomNum").text(randomNum);
 
 
-//adding crystalmeth to the tweekers
-$("#crystal1").click(function(){
-    totalScore += crystal1;
-    $("#totalScore").text(totalScore);  
-})
-$("#crystal2").click(function(){
-    totalScore += crystal2;
-    $("#totalScore").text(totalScore);   
-})
-$("#crystal3").click(function(){
-    totalScore += crystal3;
-    $("#totalScore").text(totalScore); 
-})
-$("#crystal4").click(function(){
-    totalScore += crystal4;
-    $("#totalScore").text(totalScore); 
-})
-console.log("randomNum" + randomNum);
-console.log("totalScore" + totalScore);
-//reset and win and lose
+    //adding crystalmeth to the tweekers
+    $("#crystal1").click(function () {
+        totalScore += crystal1;
+        $("#totalScore").text(totalScore);
+        numberCheck();
+    })
+    $("#crystal2").click(function () {
+        totalScore += crystal2;
+        $("#totalScore").text(totalScore);
+        numberCheck();
+    })
+    $("#crystal3").click(function () {
+        totalScore += crystal3;
+        $("#totalScore").text(totalScore);
+        numberCheck();
+    })
+    $("#crystal4").click(function () {
+        totalScore += crystal4;
+        $("#totalScore").text(totalScore);
+        numberCheck();
+    })
+    console.log("randomNum" + randomNum);
+    console.log("totalScore" + totalScore);
+    //reset and win and lose
 
-function resetGame() {
-    $("#crystal1, #crystal2, #crystal3, #crystal4, #randomNum, #totalScore").empty();
+    function resetGame() {
+        $("#crystal1, #crystal2, #crystal3, #crystal4, #randomNum, #totalScore").empty();
 
-}
-function numberCheck() {
-if ($(totalScore) === randomNum) {
-    alert("You're a fucking winner bro!!");
-    wins++;
-    resetGame();
-    
-}
-if ($("#totalScore") > randomNum) {
-    alert("You're a crystal meth loser");
-    lose++;
-    resetGame();
-}
-}
+    }
 
-numberCheck();
+    function numberCheck() {
+
+        if (totalScore === randomNum) {
+            alert("You're a winner bro!!");
+            wins++;
+            resetGame();
+
+        }
+        if (totalScore > randomNum) {
+            alert("You're a loser!!");
+            lose++;
+            resetGame();
+        }
+    }
+
+    numberCheck();
 
 });
